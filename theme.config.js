b49b786367083adc0493147486d84c03a244a1ca
br0el <96693764+br0el@users.noreply.github.com>
2022-04-16 23:37:24 -0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Whatis',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Whatis/</>,
   unstable_faviconGlyph: '👋',
 }
